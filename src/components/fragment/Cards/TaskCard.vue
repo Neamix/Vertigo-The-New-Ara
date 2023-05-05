648b6672c3ac91f98aa5ccdcb1a5402ff86a2c2e
NEAMIX <abdalrhmanhussin44@gmail.com>
2023-05-06 01:57:07 +0300

    Kanban board styling

--- src/components/fragment/Cards/TaskCard.vue
+++ src/components/fragment/Cards/TaskCard.vue
@@ -1,20 +1,24 @@
 <template>
     <div class="column-lower-container flex overflow-x-scroll">
         <div class="col flex items-start">
+            
             <!-- Start: Draggable Column -->
-            <Draggable v-model="columns" tag="ul" group="columns" item-key="id" class="flex" @change="log()" :animation="200">
+            <Draggable v-model="columns" tag="ul" group="columns" item-key="id" class="flex" :clone="false"  style="touch-action: pan-x;" :animation="400" :force-fallback="true" :scroll-sensitivity="10"  @change="log()">
                 <template #item="{element: col}">
-                    <div class="column flex-1 mr-4  w-100 min-h-16 px-3 rounded-md min-w-v_300px h-fit">
-                        <label class=" text-v_14 ">{{ col.name }}</label>
+                    <div class="column flex-1 mr-4  w-100 min-h-16 px-3 rounded-md min-w-v_300px h-fit bg-dark-200 p-2 overflow-y-hidden touch-pan-x ">
+                        <div class="column-name">
+                            <label class="text-v_14 w-full cursor-pointer" v-if="columnUnerChange != col.id" @click="columnUnerChange = col.id;columnNewName = col.name">{{ col.name }}</label>
+                            <input type="text" class="form-control change-column-name-input bg-dark-300 p-2 h-v_35px text-white w-full" @blur="changeColumnName()" v-if="columnUnerChange == col.id" v-model="columnNewName">
+                        </div>
                         <div class="empty_column" v-if="false">
                             <p class="my-0 text-gray-300 text-v_12 font-normal mt-2">Add new card</p>
                         </div>
-                        <div class="cards max-h-v_70vh overflow-y-scroll">
+                        <div class="cards max-h-v_70vh overflow-y-hidden">
                             <!-- Start: Draggable Cards -->
-                            <Draggable v-model="col.card" tag="ul" drag-class="drag-card" ghost-class="ghost-class" group="cards" @start="drag=true" @end="drag=false" :item-key="col.id"  :animation="200">
+                            <Draggable v-model="col.card" tag="ul" class=" min-h-6 " drag-class="drag-card" ghost-class="ghost-class" group="cards" @start="drag=true" @end="drag=false" :item-key="col.id"  :animation="200">
                                 <template #item="{element: card}">
                                     <li>
-                                        <div class="card bg-dark-300 rounded-md mt-2 min-h-16 mr-2 inner-card">
+                                        <div class="card bg-dark-300 rounded-md mt-2 min-h-16 inner-card">
                                             <p class="text-v_13 p-3">{{ card.name }}</p>
                                         </div>
                                     </li>
@@ -29,11 +33,18 @@
                     </div>
                 </template>
             </Draggable>
-            <button class=" text-v_13  my-2 font-semi-bold mt-2">
-                <i class="fa fa-plus text-v_10 mx-2"></i>
-                <span>Add new card</span>
-            </button>
             <!-- End: Draggable Column -->
+            
+           <div class="new-column">
+                <button class="text-v_13  my-2 font-semi-bold mt-2" v-if="openColumnText == false" @click="openColumnText = true">
+                    <i class="fa fa-plus text-v_10 mx-2"></i>
+                    <span @click="openColumnText = true">Add new column</span>
+                </button>
+                <form class="bg-dark-200 rounded-md p-3 flex-1 min-w-v_200px" @submit.prevent="addNewColumn()" v-if="openColumnText">
+                    <input type="text" class="form-control addNewColumnInput bg-dark-300 p-2 h-v_35px text-white w-full" placeholder="Enter column name" v-model="newColumnName">
+                    <button class="btn btn-primary mt-2 text-v_10 h-5">Add column</button>
+                </form>
+           </div>
         </div>
     </div>
 </template>
@@ -69,8 +80,48 @@ let columns = ref([
     }
 ]);
 
-function log() {
-    console.log(columns.value)
+// Change exist column name
+let columnUnerChange = ref(null);
+let columnNewName = ref(null);
+
+function changeColumnName() {
+    // Remove Error Border
+    document.querySelector('.addNewColumnInput').classList.remove('is-invalid');
+}
+
+// Add new column
+let newColumnName = ref(null);
+let openColumnText = ref(false);
+
+function addNewColumn() {
+    // Remove Error Border
+    document.querySelector('.addNewColumnInput').classList.remove('is-invalid');
+
+    // In case there is value in input
+    if (newColumnName.value) {
+        columns.value.push({
+            id: columns.value.length + 1,
+            name: newColumnName.value,
+            card: []
+        });
+
+        // Close column text
+        openColumnText.value = false;
+    } 
+    
+    //Add Error Border
+    else {
+        document.querySelector('.addNewColumnInput').classList.add('is-invalid');
+    }
+}
+
+function closeColumnModalOnClickOutSide(event) {
+    let el = this.$refs.dropdown;
+    let target = event.target;
+
+    if (el !== target && !el.contains(target)){
+        this.showMenu = false
+    }
 }
 
 </script>
--- src/components/laytouts/register.vue
+++ src/components/laytouts/register.vue
@@ -1,5 +1,62 @@
 <template>
-    <div class="tabs">
-        <router-view></router-view>
+    
+    <div class="tabs container">
+        <div class="header mt-10 mb-5 flex justify-between w-full">
+            <img src="@/assets/images/logos/logo_light.png" class=" w-36 mr-3 " alt="logo">
+            <button class="flex text-v_13  items-center">
+                <span class="font-bold">Already have account</span>
+                <i class="fa-solid fa-chevron-right text-v_10 mx-2 pt-0.5"></i>
+            </button>
+        </div>
+        <div class="bullets grid grid-cols-4 w-full pt-16">
+            <div class="bullet flex">
+                <div class="bullet_value rounded-full bg-dark-200 w-v_45px h-v_45px flex justify-center items-center" :class="{'bullet_active':   currentName == 'CompanyInfo' ||  currentName == 'PersonalInfo'}">
+                    <p class=" font-bold">1</p>
+                </div>
+                <div class="company_info_bullet mx-3">
+                    <p class=" text-v_14 ">Company information</p>
+                    <span class="block text-v_12 text-gray-300 capitalize">Add your company information</span>
+                </div>
+            </div>
+            <div class="bullet flex">
+                <div class="bullet_value rounded-full bg-dark-200 w-v_45px h-v_45px flex justify-center items-center" :class="{'bullet_active':  currentName == 'PersonalInfo'}">
+                    <p class=" font-bold ">2</p>
+                </div>
+                <div class="company_info_bullet mx-3">
+                    <p class=" text-v_14 ">Perosnal Information</p>
+                    <span class="block text-v_12 text-gray-300 capitalize">Add your personal information</span>
+                </div>
+            </div>
+            <div class="bullet flex">
+                <div class="bullet_value rounded-full bg-dark-200 w-v_45px h-v_45px flex justify-center items-center">
+                    <p class=" font-bold ">3</p>
+                </div>
+                <div class="company_info_bullet mx-3">
+                    <p class=" text-v_14 ">Review</p>
+                    <span class="block text-v_12 text-gray-300">Review your data</span>
+                </div>
+            </div>
+            <div class="bullet flex">
+                <div class="bullet_value rounded-full bg-dark-200 w-v_45px h-v_45px flex justify-center items-center">
+                    <p class=" font-bold ">3</p>
+                </div>
+                <div class="company_info_bullet mx-3">
+                    <p class=" text-v_14 ">Pay & Checkout</p>
+                </div>
+            </div>
+        </div>
+        <router-view :key="$route.path"></router-view>
     </div>
 </template>
+
+<script setup>
+    import { useRouter, useRoute } from 'vue-router';
+    import { useRegisterStore } from '../../stores/RegisterStore';
+    import { computed } from 'vue';
+
+    let currentName =  computed(() => {
+        return useRegisterStore().currentRegisterName
+    });
+
+
+</script>
