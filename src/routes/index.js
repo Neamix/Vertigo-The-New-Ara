2e3856d26e204e9bcefa88776772a3470784df16
NEAMIX <abdalrhmanhussin44@gmail.com>
2023-05-06 01:45:03 +0300

    add register routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,6 +5,12 @@ import { createRouter,createWebHistory } from "vue-router";
 import masterLayout from '../components/laytouts/master.vue';
 import authLayout from '../components/laytouts/auth.vue';
 import registerLayout from '../components/laytouts/register.vue';
+import NotFound from '../Pages/Main/404.vue';
+
+// Register Component
+import CompanyInfo from '../Pages/Main/Register/CompanyInfo.vue';
+import PersonalInfo from '../Pages/Main/Register/PersonalInfo.vue';
+import Checkout from '../Pages/Main/Register/Checkout.vue';
 
 // Routing importing
 import masterRoutes from "./master";
@@ -27,9 +33,32 @@ let router = createRouter({
         {
             path: '/register',
             component: registerLayout,
-            name: 'login'
+            name: 'register',
+            children: [
+                {
+                    path: 'company',
+                    component: CompanyInfo,
+                    name: 'CompanyInfo'
+                },
+                {
+                    path: 'personal',
+                    component: PersonalInfo,
+                    name: 'PersonalInfo'
+                },
+                {
+                    path: 'checkout',
+                    component: Checkout,
+                    name: 'Checkout'
+                }
+            ]
+        },
+        { 
+            path: '/:pathMatch(.*)', 
+            name: 'not-found', 
+            component: NotFound 
         }
     ]
 });
+
 
 export default router;
