c3c8ddcf252f399509403b5ce8b4432eb031876b
NEAMIX <abdalrhmanhussin44@gmail.com>
2023-05-29 13:03:59 +0300

    add auth routes

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,11 +1,32 @@
 import Login from '../Pages/Auth/Login.vue';
+import Forget from '../Pages/Auth/Passwords/Forget.vue';
+import Otp from '../Pages/Auth/Passwords/Otp.vue';
+import Reset from '../Pages/Auth/Passwords/Reset.vue';
+
 
 
 // Auth Routes
-let authRoutes = [{
-    path: 'login',
-    name: 'login',
-    component: Login
-}]
+let authRoutes = [
+    {
+        path: 'login',
+        name: 'login',
+        component: Login
+    },
+    {
+        path: 'forget',
+        name: 'forget',
+        component: Forget
+    },
+    {
+        path: 'otp',
+        name: 'otp',
+        component: Otp
+    },
+    {
+        path: 'reset',
+        name: 'reset',
+        component: Reset
+    }
+]
 
 export default authRoutes;
