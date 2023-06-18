0c77c8ee17107c85f64b4732fb9ebf9a95b35d65
Neamix <abdalrhmanhussin44@gmail.com>
2023-06-18 19:52:07 +0300

    Main Error Pages

---
+++ src/Pages/Main/Errors/500.vue
@@ -0,0 +1,13 @@
+<template>
+	<section class="flex items-center h-full dark:text-gray-100 flex items-center h-screen">
+		<div class="container flex flex-col items-center justify-center px-5 mx-auto my-8">
+			<div class="max-w-md text-center">
+				<h2 class="mb-8 font-extrabold text-v_23 text-white" style="font-size: 100px;">
+                    500 : (
+                </h2>
+				<p class="text-2xl font-semibold md:text-3xl">Our Server Down</p>
+				<p class="mt-4 mb-8 dark:text-gray-400">Hello there, Our server is down right now we are trying to fix the problem ASAP</p>
+			</div>
+		</div>
+	</section>
+</template>
