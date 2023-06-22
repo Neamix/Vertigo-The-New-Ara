c053a0301d6b4ead4c386b86b5e8579d02ac7ba7
Neamix <abdalrhmanhussin44@gmail.com>
2023-06-22 10:56:44 +0300

    Add Pusher Event Listeners

--- src/stores/PusherStore.js
+++ src/stores/PusherStore.js
@@ -1,6 +1,8 @@
 import { defineStore } from "pinia";
 import axios from 'axios';
 import { useAuthStore } from "./AuthStore";
+import Pusher from 'pusher-js';
+import { useMemberStore } from '@/stores/MembersStore';
 
 export const usePusherStore = defineStore('pusher',{
     getters: {
@@ -10,9 +12,73 @@ export const usePusherStore = defineStore('pusher',{
         getUserStatus() {
             return useAuthStore().user.status;
         },
+        pusherInstatce() {
+            let pusher = new Pusher(import.meta.env.VITE_PUSHER_APP_KEY, {
+                authEndpoint: import.meta.env.VITE_BACKEND_BROADCAST_AUTH,
+                cluster: 'sa1',
+                auth:{
+                    headers:{
+                        'Authorization': 'Bearer ' + this.getBearerToken,
+                    },
+                },
+                encrypted: true
+            });
+
+            return pusher;
+        }
     },
 
     actions: {
+        initPusher() {
+            // Init Pusher
+            let pusher = this.pusherInstatce;
+            let AuthStore = useAuthStore();
+            let memberStore = useMemberStore();
+
+            // let company_channel = useAuthStore().user.
+            let channel_name = 'presence-company.'+AuthStore.user.company_id;
+            let channel = pusher.subscribe(channel_name);
+
+            // Add Active Members When User Subscribe To The Channel
+            channel.bind('pusher:subscription_succeeded', function(member) {
+                channel.members.each(function (member) {
+                    memberStore.activeMembers.push(member.info.user_id)
+                });
+            });
+
+            // Add Subscriped Members To The Array Of Active
+            channel.bind("pusher:member_added", (member) => {
+                memberStore.activeMembers.push(member.id)
+            });
+
+            // Remove Leaved Members From The Array Of Active
+            channel.bind("pusher:member_removed", (member) => {
+                let memberIndex = memberStore.activeMembers.findIndex((x) => x == member.id)
+                memberStore.activeMembers.splice(memberIndex,1);
+            });
+
+            // Bind Event Status
+            channel.bind('status', function(data) {
+                let members = memberStore.members;
+                let user_id = data.user.user_id;
+
+                // Find Index of User
+                let index = members.findIndex((x) =>  x.id == user_id);
+                
+                // In Case This User Exist In The Array Change Its Status
+                if ( index > -1 ) {
+                    members[index].status.id = data.user.status_id;
+                }
+            });
+        },
+        unSubscripeCompanyChannel(company_id) {
+            let pusher = this.pusherInstatce;
+            pusher.unsubscribe('presence-company.'+company_id)
+        },
+        subscribeCompanyChannel(company_id) {
+            let pusher = this.pusherInstatce;
+            pusher.subscribe('presence-company.'+company_id);
+        },
         changeStatus() {
             return axios({
                 url: import.meta.env.VITE_BACKEND_URL,
