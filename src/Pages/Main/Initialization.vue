bbd3ea3d7ca82a6da9d45e6b70681d06afce46cd
Neamix <abdalrhmanhussin44@gmail.com>
2023-09-06 14:19:47 +0300

    add initialization loader

---
+++ src/Pages/Main/Initialization.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="w-full h-screen bg-dark-300  flex justify-center items-center flex-col fixed z-50" v-if="globalStore.initializationLoader">
+        <Transition name="movedown" appear>
+            <img src="@assets/images/logos/logo_light.png" class=" w-44 relative " alt="logo" v-if="globalStore.initializationLoader">
+        </Transition>
+        <Transition name="moveup" appear>
+            <div class="container w-60 mt-3 relative flex justify-center" v-if="globalStore.initializationLoader">
+                <span class="loading block mx-3"></span>
+                <span>Initialization of workspace</span>
+            </div>
+        </Transition>
+    </div>
+</template>
+
+<script setup>
+import { useGlobalStore } from '../../stores/GlobalStore';
+
+// Define Store
+const globalStore = useGlobalStore()
+</script>
--- src/components/laytouts/master.vue
+++ src/components/laytouts/master.vue
@@ -4,30 +4,29 @@
             <!-- AsideNav -->
             <AsideNav @toggleSideNav="toggleSideNav"></AsideNav>
             <!-- Main View -->
-                <div class="pl-3 main_content min-h-screen w-full" :class="{'closed_nav': closed_nav}">
-                    <Page class="w-full">
-                        <MainHeader>
-                            <template #title>
-                                {{ useGlobalStore().page  }}
-                            </template>
-                        </MainHeader>
-                        <router-view v-slot="{Component}">
-                            <component :is="Component" v-if="useGlobalStore().renderComponent"></component>
-                        </router-view>
-                    </Page>
-                </div>
-            <!-- Loader  -->
+            <div class="pl-3 main_content min-h-screen w-full" :class="{'closed_nav': closed_nav}">
+                <Page class="w-full">
+                    <MainHeader>
+                        <template #title>
+                            {{ useGlobalStore().page  }}
+                        </template>
+                    </MainHeader>
+                    <router-view v-slot="{Component}">
+                        <component :is="Component" v-if="useGlobalStore().renderComponent"></component>
+                    </router-view>
+                </Page>
+            </div>
         </div>
     </div>
 </template>
 
 <script setup>
 import AsideNav from '../fragment/AsideNav.vue';
-import { ref,computed, watch } from 'vue';
+import { ref,computed, watch, onMounted } from 'vue';
 import MainHeader from '@/components/fragment/MainHeader.vue';
 import Page from '@/components/fragment/Pages/Page.vue';
 import { useGlobalStore } from "@/stores/GlobalStore";
-import { usePusherStore } from '../../stores/PusherStore';
+import { usePusherStore } from '@/stores/PusherStore';
 
 /** Define Stores */
 let globalStore = useGlobalStore();
@@ -37,7 +36,6 @@ let closed_nav = computed(() => globalStore.closed_nav);
 
 watch(() => globalStore.closed_nav,(value) => {
     closed_nav = value;
-
 });
 
 /*** Subscripe Channels */
@@ -52,5 +50,11 @@ const renderComponent = ref(true);
 
 pusherStore.initPusher();
 
+
+onMounted(() => {
+    setTimeout(() => {
+        globalStore.initializationLoader = false;
+    },1000)
+})
 
 </script>
